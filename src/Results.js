561607eb81436b4bdaf02306e6f6958e39df6b01
Fatima Tuz Zehra <57491064+fatimacodes2001@users.noreply.github.com>
2021-08-10 13:18:20 +0500

    Added files

---
+++ src/Results.js
@@ -0,0 +1,32 @@
+/*global React ReactDOM*/
+
+import Pet from "./Pet";
+import React from "react"
+
+import regeneratorRuntime from "regenerator-runtime";
+
+const Results = ({ pets }) => {
+  return (
+    <div className="search">
+      {!pets.length ? (
+        <h1>No Pets Found</h1>
+      ) : (
+        pets.map((pet) => {
+          return (
+            <Pet
+              animal={pet.animal}
+              key={pet.id}
+              name={pet.name}
+              breed={pet.breed}
+              images={pet.images}
+              location={`${pet.city}, ${pet.state}`}
+              id={pet.id}
+            />
+          );
+        })
+      )}
+    </div>
+  );
+};
+
+export default Results;
