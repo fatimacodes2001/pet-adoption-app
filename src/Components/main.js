836e0f866bea026cbd38f3763576ee1a6d954fff
Fatima Tuz Zehra <57491064+fatimacodes2001@users.noreply.github.com>
2022-07-31 01:31:27 +0500

    Updated directory structure

---
+++ src/Components/main.js
@@ -0,0 +1,44 @@
+/*global React ReactDOM*/
+
+
+import React from "react"
+import ReactDOM  from "react-dom";
+import SearchParams from "./SearchParams"
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { StrictMode } from "react/cjs/react.production.min";
+import Details  from "./Details";
+
+
+
+const App = () => {
+  return (
+
+    <div>
+
+      <Router>
+        <header>
+        <Link to="/"> Adopt Me </Link>
+        </header>
+
+      <Switch>
+
+        <Route path="/details/:id">
+          <Details />
+        </Route>
+
+        <Route path="/"> 
+        < SearchParams />
+        </Route>
+
+      </Switch>
+
+      </Router>
+    </div>
+
+
+  );
+};
+
+ReactDOM.render(
+<App/>
+, document.getElementById("root"));
